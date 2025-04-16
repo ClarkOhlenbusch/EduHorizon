eb75089af5a61674b1a1771482dfd51526824e31
clarkohlenbusch <41375803-clarkohlenbusch@users.noreply.replit.com>
2025-04-16 00:12:22 +0000

    Restored to 'ee4b76eb5a4a56a6d9634e3626637eda6e2e5ce0'
    Replit-Restored-To: ee4b76eb5a4a56a6d9634e3626637eda6e2e5ce0

--- build-windows.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Building the application...
-npx vite build && npx esbuild server/index.ts --platform=node --packages=external --bundle --format=esm --outdir=dist
-echo Build complete!
